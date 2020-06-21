889c5ed93eac60f7fd7011f942c0381203e06ea4
OliverHFU <oliver.leisinger@hs-furtwangen.de>
2020-06-21 17:50:20 +0200

    Added utils andWorking on UI

--- Gruppe5.2-Lanchester-Modell-Simulation/src/UI/SimulationMain.java
+++ Gruppe5.2-Lanchester-Modell-Simulation/src/UI/SimulationMain.java
@@ -1,10 +1,37 @@
 package UI;
 
+import java.util.Timer;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import UI._UI_Constants;
+import utils.ApplicationTime;
+import utils.FrameUpdate;
+
 public class SimulationMain {
-
+	private static JFrame frame;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ApplicationTime animThread = new ApplicationTime();
+		animThread.start();
 
+		CreateFrame(animThread);
+		
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new FrameUpdate(frame), 100, _UI_Constants.TPF);
 	}
 
+	private static void CreateFrame(ApplicationTime thread) {
+		
+		//Create a new frame
+		frame = new JFrame("Mathematik und Simulation");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Add a JPanel as the new drawing surface
+		JPanel panel = new WindowContent(thread);
+		frame.add(panel);
+		frame.pack(); //adjusts size of the JFrame to fit the size of it's components
+		frame.setVisible(true);
+	}
 }
---
+++ Gruppe5.2-Lanchester-Modell-Simulation/src/UI/WindowContent.java
@@ -0,0 +1,33 @@
+package UI;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import utils.ApplicationTime;
+
+public class WindowContent extends JPanel {
+	
+	//panel has a single time tracking thread associated with it
+	private ApplicationTime t;
+	private double time;
+	
+	public WindowContent(ApplicationTime thread) {
+		this.t = thread;
+	}
+	
+	//set this panel's preferred size for auto-sizing the container JFrame
+	public Dimension getPreferredSize() {
+		return new Dimension(_UI_Constants.Window_Width, _UI_Constants.Window_Height);
+	}
+	
+	//drawing operations should be done in this method
+	@Override protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		time = t.GetTimeInSeconds();
+		 
+	}
+}
+
